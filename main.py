9c8e7cda5d7a08766b5eec023f4c539d085b9ff0
Your Name <deckerwill7@gmail.com>
2024-01-26 07:46:27 -0600

    clean

--- main.py
+++ main.py
@@ -4,19 +4,16 @@ from config import IMAGES_DIR
 
 # some brain plots
 
-p = plot_rois(roi=34, cmap='YlOrBr')
+p1 = plot_rois(roi=34, cmap='YlOrBr')
 plt.savefig(f'{IMAGES_DIR}/ut_univar_roi_1.png')
-pp = plot_rois(roi=34, cmap='afmhot')
+p2 = plot_rois(roi=34, cmap='afmhot')
 plt.savefig(f'{IMAGES_DIR}/ut_univar_roi_2.png')
-ppp = plot_rois(roi=25, cmap='winter')
+p3 = plot_rois(roi=25, cmap='winter')
 plt.savefig(f'{IMAGES_DIR}/ut_univar_roi_3.png')
-pppp = plot_rois(roi=25, cmap='afmhot')
+p4 = plot_rois(roi=25, cmap='afmhot')
 plt.savefig(f'{IMAGES_DIR}/ut_univar_roi_4.png')
-r = list(np.arange(1, 53))
-ppppp = plot_rois(roi=r, cmap='afmhot')
-plt.savefig(f'{IMAGES_DIR}/ut_univar_roi_6.png')
 
-pppppp = plot_glass(cmap='afmhot', threshold=2)
+p5 = plot_glass(cmap='afmhot', threshold=2)
 plt.savefig(f'{IMAGES_DIR}/glass_1.png')
-ppppppp = plot_glass(cmap='afmhot', threshold=1.5)
+p6 = plot_glass(cmap='afmhot', threshold=1.5)
 plt.savefig(f'{IMAGES_DIR}/glass_2.png')
