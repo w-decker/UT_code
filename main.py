3a5de5d757b9f48e8843c06b72e557b6dd3a53b4
Your Name <deckerwill7@gmail.com>
2024-01-26 07:40:11 -0600

    implement changes from .gitignore update

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
 from utils import *
 import matplotlib.pyplot as plt
+from config import IMAGES_DIR
 
 # some brain plots
 
 p = plot_rois(roi=34, cmap='YlOrBr')
-plt.savefig('images/ut_univar_roi_1.png')
+plt.savefig(f'{IMAGES_DIR}/ut_univar_roi_1.png')
 pp = plot_rois(roi=34, cmap='afmhot')
-plt.savefig('images/ut_univar_roi_2.png')
+plt.savefig(f'{IMAGES_DIR}/ut_univar_roi_2.png')
 ppp = plot_rois(roi=25, cmap='winter')
-plt.savefig('images/ut_univar_roi_3.png')
+plt.savefig(f'{IMAGES_DIR}/ut_univar_roi_3.png')
 pppp = plot_rois(roi=25, cmap='afmhot')
-plt.savefig('images/ut_univar_roi_4.png')
+plt.savefig(f'{IMAGES_DIR}/ut_univar_roi_4.png')
 r = list(np.arange(1, 53))
 ppppp = plot_rois(roi=r, cmap='afmhot')
-plt.savefig('images/ut_univar_roi_6.png')
+plt.savefig(f'{IMAGES_DIR}/ut_univar_roi_6.png')
 
 pppppp = plot_glass(cmap='afmhot', threshold=2)
-plt.savefig('images/glass_1.png')
+plt.savefig(f'{IMAGES_DIR}/glass_1.png')
 ppppppp = plot_glass(cmap='afmhot', threshold=1.5)
-plt.savefig('images/glass_2.png')
+plt.savefig(f'{IMAGES_DIR}/glass_2.png')
